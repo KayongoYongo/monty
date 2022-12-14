aae409e8d0fb4ba93f12258e5f132a11fbbdfde8
KayongoYongo <sknsammie10@gmail.com>
2022-12-14 17:26:08 +0000

    push

--- push.c
+++ push.c
@@ -12,14 +12,12 @@ void push(stack_t **stack, unsigned int line_number)
 	stack_t *new, *tmp;
 	char *push_arg = strtok(NULL, "\n \t");
 	int pVal;
-	/*if push, tests if the push_arg was valid or not */
+
 	if (!is_int(push_arg))
 	{
 		fprintf(stdout, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
-
 	pVal = atoi(push_arg);
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
@@ -30,19 +28,17 @@ void push(stack_t **stack, unsigned int line_number)
 	new->n = pVal;
 	new->prev = NULL;
 	new->next = NULL;
-	/** checks if stack is empty **/
+
 	if ((*stack) == NULL)
 		*stack = new;
 	else if (SQ)
 	{
-		/** puts new node on top if not empty **/
 		(*stack)->prev = new;
 		new->next = *stack;
 		*stack = new;
 	}
 	else
 	{
-		/**puts new node on the bottom **/
 		tmp = *stack;
 		while (tmp->next != NULL)
 			tmp = tmp->next;
